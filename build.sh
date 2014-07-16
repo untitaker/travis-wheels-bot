321ea4f7d50a3c4cc59f0fc50ab10e177b67b485
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-07-16 20:29:42 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+set -e
 pip wheel --wheel-dir=./wheels/ ${PACKAGE}==${VERSION}
 
-curl --upload-file ./wheels/* -u $SERVER_USER:$SERVER_PASSWORD http://travis-wheels.unterwaditzer.net/upload
+if [ "$(find ./wheels/* | wc -l)" != "1" ]; then
+    echo "Too many files"
+    find ./wheels/*
+    exit 1
+fi
+
+filename="$(basename $(find ./wheels/*))"
+
+curl -F "wheel=@./wheels/$filename" -u $SERVER_USER:$SERVER_PASSWORD \
+    http://travis-wheels.unterwaditzer.net/upload
