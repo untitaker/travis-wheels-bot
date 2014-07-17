08dd6db2e70c02d5b8dd072d9ea097d6d019134c
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-07-17 22:23:01 +0200

    Don't build if package exists.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 set -e
+[ -n "$HOST" ] || HOST="http://travis-wheels.unterwaditzer.net"
+
+package_exists() {
+    curl "$HOST/wheels/" | grep "$PACKAGE-$VERSION" | grep "$(echo $TRAVIS_PYTHON_VERSION | tr -d .)"
+}
+if package_exists; then
+    echo "Package already exists."
+    exit
+fi
+
+pip install wheel
 pip wheel --no-deps --wheel-dir=./wheels/ ${PACKAGE}==${VERSION}
 
 if [ "$(find ./wheels/* | wc -l)" != "1" ]; then
@@ -9,6 +20,5 @@ if [ "$(find ./wheels/* | wc -l)" != "1" ]; then
 fi
 
 filename="$(basename $(find ./wheels/*))"
-[ -n "$HOST" ] || HOST="http://travis-wheels.unterwaditzer.net"
 
 curl -F "wheel=@./wheels/$filename" -F "secret_key=$SECRET_KEY" "$HOST/upload"
